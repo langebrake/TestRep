e0670a5bf84914616a7af140d6fb155f0ea9e026
Hendrik <hlangebrake@uos.de>
2016-04-12 19:16:04 +0200

    For Loop implementation

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -3,5 +3,8 @@ public class Main {
 	public static void main(String[] args){
 		System.out.println("HELLO WORLD!");
 		System.out.println("Output Change");
+		for(int i = 2; i<10;i++){
+			
+		}
 	}
 }
