ba07742277d56b0d830e6b1b00ed5067821042d3
Hendrik <hlangebrake@uos.de>
2016-04-18 18:42:32 +0200

    demo test extended

--- Test/src/GraphTest.java
+++ Test/src/GraphTest.java
@@ -13,6 +13,18 @@ public class GraphTest {
 			Plugin inputPlugin = new MidiInputPlugin(inputModule);
 			inputModule.setPlugin(inputPlugin);
 			System.out.println("FINISHED");
+			
+			
+			Module defaultModule = new Module(Engine.load());
+			Plugin defaultPlugin = new DefaultPlugin(defaultModule);
+			defaultModule.setPlugin(defaultPlugin);
+			
+			
+			
+			defaultModule.connectInput(inputModule.getOuput(0));
+			
+			
+			defaultModule.close();
 			inputModule.close();
 		}catch(Exception e){
 			e.printStackTrace();
